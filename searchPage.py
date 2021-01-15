1f1ec092d2dcfdc284cb971fcab6f70810fd668c
ahmettoktasdot <67964367+ahmettoktasdot@users.noreply.github.com>
2021-01-15 16:51:04 +0300

    Update searchPage.py

--- searchPage.py
+++ searchPage.py
@@ -1,26 +1,14 @@
-from selenium.webdriver.common.by import By
-from pomTests.homework5.locators import Locators
+from homework5.locators import search_page_locators, parameters
+from homework5.base import BaseFunctions
+import unittest
 
-class SearchPage():
-    def __init__(self, driver):
-        self.driver = driver
 
-        self.search_textbox_locators = Locators.search_textbox_locators
-        self.search_button_locators = Locators.search_button_locators
-        self.search_secondpage_locators = Locators.search_secondpage_locators
-        self.secondpage_locators = Locators.secondpage_locators
-        self.product_locators = Locators.product_locators
-
-    def enter_search_text(self, text):
-        self.driver.find_element(By.ID, self.search_textbox_locators).send_keys(text)
-        assert (self.driver.find_element(By.ID, self.search_textbox_locators)).get_attribute("value") == text, "Az önce search ettiğin sayfada değilsin."
-
-    def click_search_button(self):
-        self.driver.find_element(By.XPATH, self.search_button_locators).click()
-
-    def click_second_page(self):
-        self.driver.find_element(By.XPATH, self.search_secondpage_locators).click()
-        assert (self.driver.find_element(By.XPATH, self.secondpage_locators)).get_attribute("text") == "2", "İkinci sayfada değilsin."
+class SearchFunctions(BaseFunctions, unittest.TestCase):
+    def search(self):
+        self.input(search_page_locators.search_textbox, parameters.search_text)
+        self.click(search_page_locators.search)
+        self.assertTrue(self.wait_for_element(search_page_locators.search_textbox).get_attribute("value"), parameters.search_text)
+        self.click(search_page_locators.second_page)
 
     def click_product(self):
-        self.driver.find_element(By.XPATH, self.product_locators).click()
+        self.click(search_page_locators.product)
