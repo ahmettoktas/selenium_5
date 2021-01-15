908ba22db6f23c3904ef9c1b14e83ac44af95d1e
ahmettoktasdot <67964367+ahmettoktasdot@users.noreply.github.com>
2021-01-15 16:50:52 +0300

    Update productPage.py

--- productPage.py
+++ productPage.py
@@ -1,30 +1,18 @@
-from selenium.webdriver.common.by import By
-from pomTests.homework5.locators import Locators
-import time
+from homework5.locators import product_page_locators
+from homework5.base import BaseFunctions
+import unittest
 
-class ProductPage():
-    def __init__(self, driver):
-        self.driver = driver
 
-        self.addToWishlist_button_locators = Locators.addToWishlist_button_locators
-        self.go_to_deleteProductWishlistPage_locators = Locators.go_to_deleteProductWishlistPage_locators
-        self.delete_from_wishlist_locators = Locators.delete_from_wishlist_locators
-        self.productText1 = Locators.productText1
-        self.productText2 = Locators.productText2
+class ProductFunctions(BaseFunctions, unittest.TestCase):
+    def add_to_wishlist(self):
+        self.click(product_page_locators.add_to_wishlist)
 
-    def addToWishlist(self):
-        self.driver.find_element(By.ID, self.addToWishlist_button_locators).click()
+    def navigate_to_wishlist_page(self):
+        product_text = self.wait_for_element(product_page_locators.product_text).text
+        self.click(product_page_locators.wishlist)
+        self.assertTrue(product_text, self.wait_for_element(product_page_locators.in_wishlist_product_text).text)
 
-    def goToDeletePage(self):
-        textProduct1_1 = (self.driver.find_element(By.XPATH, self.productText1)).get_attribute("innerHTML")
-        self.driver.find_element(By.XPATH, self.go_to_deleteProductWishlistPage_locators).click()
-        time.sleep(1)
-        textProduct2_1 = (self.driver.find_element(By.XPATH, self.productText2)).get_attribute("innerHTML")
-        assert textProduct1_1[:20] == textProduct2_1[:20], "Bu iki ürün aynı değil."
-
-    def DeleteProduct(self):
-        textProduct2_1 = (self.driver.find_element(By.XPATH, self.productText2)).get_attribute("innerHTML")
-        self.driver.find_element(By.XPATH, self.delete_from_wishlist_locators).click()
-        print(self.driver.find_element(By.XPATH, self.productText2)).get_attribute("innerHTML")
-        assert self.driver.find_element(By.XPATH, self.productText2) == textProduct2_1 , "There is a problem"
-
+    def delete_product(self):
+        product_text = self.wait_for_element(product_page_locators.in_wishlist_product_text).text
+        self.click(product_page_locators.delete)
+        self.assertFalse(product_text, self.wait_for_element(product_page_locators.in_wishlist_product_text).text)
